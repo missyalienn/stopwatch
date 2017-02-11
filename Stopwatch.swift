f90d4f413e7fea51a9fa6e0486722beddc90d920
missyalienn <allan.missy@gmail.com>
2017-02-11 15:43:21 -0500

    many updates. updated mehtods and added elapsed time

---
+++ Stopwatch.swift
@@ -0,0 +1,53 @@
+//
+//  Stopwatch.swift
+//  Stopwatch
+//
+//  Created by Missy Allan on 2/11/17.
+//  Copyright © 2017 Your School. All rights reserved.
+//
+
+import Foundation
+
+
+class Stopwatch {
+    
+    //  Declare startTime as a var and an optional, because its initial value will be set when the Stopwatch starts to track elapsed time, rather than during initialization.
+    //
+
+    private var startTime: NSDate?
+    
+    var elapsedTime: TimeInterval {
+        if let startTime = self.startTime {
+            return -startTime.timeIntervalSinceNow
+        } else {
+            return 0
+        }
+    }
+
+    
+    var isRunning: Bool {
+        return startTime != nil
+    }
+    
+    
+    func start() {
+         startTime = NSDate()
+        
+    }
+    
+    func stop() {
+        startTime = nil
+        //at the press of stop, startTime has no value
+    }
+    
+
+    
+//    Discuss how, when a Stopwatch starts, it keeps track of its start time by assigning a new NSDate to its startTime property.
+//     Explain the assignment of nil to an optional property to indicate that it has "no value."
+    
+//    Why is it important to make the start time a private property, and the elapsed time a read-only, computed property?
+
+    
+    
+    
+ }
--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    let stopwatch = Stopwatch()  // property will be assigned default Stopwatch value when ViewController is initialized
+    private var startTime: NSDate?
+    @IBOutlet weak var elapsedTimeLabel: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,32 @@ class ViewController: UIViewController {
     }
     
     
+    
+    @IBAction func startButtonTapped(_ sender: Any) {
+        print("starting stopwatch")
+        Timer.scheduledTimer(timeInterval: 1.0, target: self,
+                             selector: #selector(ViewController.updateElapsedTimeLabel(_:)), userInfo: nil, repeats: true)
+          stopwatch.start()
+        
+
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        print("Stopping the watch.")
+        stopwatch.stop()
+    }
+
+    
+    func updateElapsedTimeLabel(_ timer: Timer) {
+        print("updating...")
+        if stopwatch.isRunning {
+            elapsedTimeLabel.text = "\(stopwatch.elapsedTime)"
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+
 }
 
 
